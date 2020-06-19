864c88e925d9b4a61a5f9fb2db054470bda446a1
gcristini <gabriele.cristini@datalogic.com>
2020-06-19 15:46:36 +0200

    code of main.py into TemperatureTextSX5.py. Main.py used for user interface app.

--- Main.py
+++ Main.py
@@ -3,95 +3,57 @@
 import sys
 import os
 from Parser import Parser
-from SX5_Manager import SX5_Manager
-from ParseXml import XmlDictConfig
-from xml.etree import ElementTree
-from ImageManager import ImageManager
-from GlobalVariables import GlobalVariables as gv
-from GlobalVariables import GlobalSettings as gs
+from TemperatureTestSX5 import TemperatureTestSx5
 from Debug import Debug as dbg
+from GlobalVariables import GlobalSettings as gs
 
-
-
-class TemperatureTestSx5(object):
+class Main(object):
     """ """
+
 
     # ************************************************* #
     # **************** Private Methods **************** #
     # ************************************************* #
     def __init__(self):
-        # Global settings/variables
-        self._gv_scan_engine = gv().scan_engine_dict
+        """ Constructor """
+
+        # Dictionary for input line command.
+        self._parse_dict = {
+            '-a': self._parse_fun_a,
+            '-b': self._parse_fun_b,
+        }
+
+        # Initialize Parser with input arguments and parser dictionary
+        self._parser = Parser(input_args=sys.argv[1:],
+                              parse_dict=self._parse_dict)
+
+        self._TemperatureTestSX5 = TemperatureTestSx5()
+
         self._gs = gs().global_settings
 
-        self._config_file = 'Config.xml'
-        self._config_dict = None
-        self._SX5 = None
-        self._image_manager = None
+    # ******** Parser Dictionary Functions ******** #
+    def _parse_fun_a(self, argv):
+        """ Parser function [-id]: set input directory of ImageManager """
+        if argv:
+            pass
+        else:
+            pass
 
-        pass
-
-    def parse_config_file(self):
-        """ """
-        # Read Configuration file and store it into dictionary
-        self._config_dict = XmlDictConfig(ElementTree.parse(self._config_file).getroot())
-
-        pass
-
-    def init_sx5(self):
-        """ """
-
-        dbg.debug(print, self._config_dict['SX5']['adb_pull_base_dir'], debug=self._gs['debug'])
-
-        # Init an SX5 instance
-        self._SX5 = SX5_Manager(scan_engine=self._config_dict['SX5']['scan_engine'],
-                                num_frame=self._config_dict['SX5']['num_of_frame'],
-                                num_loop=self._config_dict['SX5']['num_of_loop'],
-                                num_save_files=self._config_dict['SX5']['num_of_save_files'],
-                                frame_storage_dir=self._config_dict['SX5']['frame_storage_dir'],
-                                adb_pull_dir=self._config_dict['SX5']['adb_pull_base_dir'],
-                                )
-        pass
-
-    def init_thermal_chamber(self):
-        pass
-
-    def init_image_manager(self):
-
-        self._image_manager = ImageManager(input_dir=self._config_dict['SX5']['adb_pull_base_dir'],
-                                           output_dir=self._config_dict['ImageManager']['output_dir'],
-                                           img_width=self._gv_scan_engine[self._config_dict['SX5']['scan_engine']]['width'],
-                                           img_height=self._gv_scan_engine[self._config_dict['SX5']['scan_engine']]['height'],
-                                           out_ext=self._config_dict['ImageManager']['img_format'])
-
-        pass
-
+    def _parse_fun_b(self, argv):
+        """ Parser function [-id]: set input directory of ImageManager """
+        if argv:
+            pass
+        else:
+            pass
 
     # ************************************************ #
     # **************** Public Methods **************** #
     # ************************************************ #
     def main(self):
         """ """
-        # ----------------- INIT ----------------- #
-        # Parse input arguments
-        #self._parser.parse_input_args()
-
-        # Parse config file
-        self.parse_config_file()
-        # Set SX5
-        self.init_sx5()
-        # Init Image Manager
-        self.init_image_manager()
-
-        # ----------------- RUN ----------------- #
-        self._SX5.run_scan_engine()
-        self._SX5.pull_images()
-
-        self._image_manager.convert_images(show=False, save=True)
-
-        self._SX5.clear_frame_storage_dir()
+        dbg.debug(print, "Start Main", debug=self._gs['debug'])
+        self._TemperatureTestSX5.run_test()
 
 
-
-TemperatureTestSx5().main()
+Main().main()
 
---
+++ TemperatureTestSX5.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from Parser import Parser
+from SX5_Manager import SX5_Manager
+from ParseXml import XmlDictConfig
+from xml.etree import ElementTree
+from ImageManager import ImageManager
+from GlobalVariables import GlobalVariables as gv
+from GlobalVariables import GlobalSettings as gs
+from Debug import Debug as dbg
+
+
+
+class TemperatureTestSx5(object):
+    """ """
+
+    # ************************************************* #
+    # **************** Private Methods **************** #
+    # ************************************************* #
+    def __init__(self):
+        # Global settings/variables
+        self._gv_scan_engine = gv().scan_engine_dict
+        self._gs = gs().global_settings
+
+        self._config_file = 'Config.xml'
+        self._config_dict = None
+        self._SX5 = None
+        self._image_manager = None
+
+        pass
+
+    def parse_config_file(self):
+        """ """
+        # Read Configuration file and store it into dictionary
+        self._config_dict = XmlDictConfig(ElementTree.parse(self._config_file).getroot())
+
+        pass
+
+    def init_sx5(self):
+        """ """
+
+        dbg.debug(print, self._config_dict['SX5']['adb_pull_base_dir'], debug=self._gs['debug'])
+
+        # Init an SX5 instance
+        self._SX5 = SX5_Manager(scan_engine=self._config_dict['SX5']['scan_engine'],
+                                num_frame=self._config_dict['SX5']['num_of_frame'],
+                                num_loop=self._config_dict['SX5']['num_of_loop'],
+                                num_save_files=self._config_dict['SX5']['num_of_save_files'],
+                                frame_storage_dir=self._config_dict['SX5']['frame_storage_dir'],
+                                adb_pull_dir=self._config_dict['SX5']['adb_pull_base_dir'],
+                                )
+        pass
+
+    def init_thermal_chamber(self):
+        pass
+
+    def init_image_manager(self):
+
+        self._image_manager = ImageManager(input_dir=self._config_dict['SX5']['adb_pull_base_dir'],
+                                           output_dir=self._config_dict['ImageManager']['output_dir'],
+                                           img_width=self._gv_scan_engine[self._config_dict['SX5']['scan_engine']]['width'],
+                                           img_height=self._gv_scan_engine[self._config_dict['SX5']['scan_engine']]['height'],
+                                           out_ext=self._config_dict['ImageManager']['img_format'])
+
+        pass
+
+
+    # ************************************************ #
+    # **************** Public Methods **************** #
+    # ************************************************ #
+    def run_test(self):
+        """ """
+        # ----------------- INIT ----------------- #
+        # Parse input arguments
+        #self._parser.parse_input_args()
+
+        # Parse config file
+        self.parse_config_file()
+        # Set SX5
+        self.init_sx5()
+        # Init Image Manager
+        self.init_image_manager()
+
+        # ----------------- RUN ----------------- #
+        self._SX5.run_scan_engine()
+        self._SX5.pull_images()
+
+        self._image_manager.convert_images(show=False, save=True)
+
+        self._SX5.clear_frame_storage_dir()
+
+
